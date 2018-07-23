91d44cd74344c0b9dd952bba0575b16e08269257
rainboyan <rainboyan@gmail.com>
2018-07-23 15:05:07 +0800

    Initial commit

---
+++ complete/src/main/java/org/springdev/guides/helloworld/HelloWorldApplication.java
@@ -0,0 +1,12 @@
+package org.springdev.guides.helloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldApplication.class, args);
+	}
+}
